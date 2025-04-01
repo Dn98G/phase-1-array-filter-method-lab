76ed2bf18d5398ebee5623c90acdf810f460061f
Denis Cheruiyot <Cheruiyotkp974@gmail.com>
2025-04-01 12:29:06 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+console.log(findMatching(drivers, "Sammy"));
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+  );
+}
+
+console.log(fuzzyMatch(drivers, "sa"));
+
+function matchName(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.name.toLowerCase() === name.toLowerCase()
+  );
+}
